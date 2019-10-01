30da1fa7cf2f9262660126aaabef5b3f23dc990f
Anastasia Sakun <sakun_nastya@mail.ru>
2019-10-01 13:41:34 +0300

    Triangle and UnitTests are ready

---
+++ TriangleAndUnitTests/Triangle/Triangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public static bool FixingTriangle(double a, double b, double c)
+        {
+           return (a < b + c && b < a + c && c < a + b) && (a > 0 && b > 0 && c > 0);
+        }
+    }
+}
