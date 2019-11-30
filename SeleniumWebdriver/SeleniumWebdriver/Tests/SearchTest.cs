93f6da022cf112890dd5cd9baf50b6ca57e441af
A <43045804+NastyaSakun@users.noreply.github.com>
2019-11-30 22:32:02 +0300

    fixed

--- SeleniumWebdriver/SeleniumWebdriver/Tests/SearchTest.cs
+++ SeleniumWebdriver/SeleniumWebdriver/Tests/SearchTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -8,36 +8,39 @@ namespace SeleniumWebdriver.Tests
     [TestFixture]
     public class Tests : TestBase
     {
+        private const string errorInSearchWithoutPrivateInformation = "Select Departure City";
+        private const string errorInWorkWithCheckIn = "The information submitted does not match any itinerary.Please verify the information is correct and try again.";
+
         [Test]
-        public void SearchWithEmptyBoxes()
+        public void SearchWithoutPrivateInformation()
         {
             var findButton = GetWebElementByXPath("//input[@class='bookbtn at-element-click-tracking']");
             findButton.Click();
 
             var errorMessage = GetWebElementByXPath("//div[@id='view-origin-station']");
             string error = errorMessage.Text;
-            Assert.AreEqual("Select Departure City", error);
+            Assert.AreEqual(errorInSearchWithoutPrivateInformation, error);
         }
 
         [Test]
         public void WorkWithCheckIn()
         {
 
-            var checkInButton = GetWebElementByXPath("//* [@class = 'spiceFare']");
-            checkInButton.Click();
+            var checkIn = GetWebElementByXPath("//* [@class = 'spiceFare']");
+            checkIn.Click();
 
-            var ticketNumberBox = GetWebElementByXPath("//*[@id='BookingRetrieveInputSearch1WebCheckinSearchView_ConfirmationNumber']");
-            ticketNumberBox.SendKeys("P52FKC");
+            var ticketNumber = GetWebElementByXPath("//*[@id='BookingRetrieveInputSearch1WebCheckinSearchView_ConfirmationNumber']");
+            ticketNumber.SendKeys("P52FKC");
 
-            var surnameBox = GetWebElementByXPath("//*[@id='BookingRetrieveInputSearch1WebCheckinSearchView_CONTACTEMAIL1']");
-            surnameBox.SendKeys("Sakun");
+            var surname = GetWebElementByXPath("//*[@id='BookingRetrieveInputSearch1WebCheckinSearchView_CONTACTEMAIL1']");
+            surname.SendKeys("Sakun");
 
-            var searchButton = GetWebElementByXPath("//*[@id='BookingRetrieveInputSearch1WebCheckinSearchView_ButtonRetrieve']");
-            searchButton.Click();
+            var search = GetWebElementByXPath("//*[@id='BookingRetrieveInputSearch1WebCheckinSearchView_ButtonRetrieve']");
+            search.Click();
 
             var errorMessage = GetWebElementByXPath("//div[@id='errorSectionContent']");
             string error = errorMessage.Text;
-            Assert.IsNotNull(error);
+            Assert.AreEqual(errorInWorkWithCheckIn, error);
         }
 
 
